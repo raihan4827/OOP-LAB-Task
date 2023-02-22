477c7e8df9afbb20c5df9f7d9df905949f39a26c
Raihan Hossain <124167171+raihan4827@users.noreply.github.com>
2023-02-22 19:17:15 +0600

    room information

---
+++ Lab work - 06/Room.java
@@ -0,0 +1,26 @@
+public class Room {
+    int roomNo;
+    String roomType;
+    float roomArea;
+    boolean acMachine;
+    Room() {
+        System.out.println("Hello");
+    }
+    Room(int roomNo, String roomType, float roomArea, boolean acMachine) {
+        this.roomNo = roomNo;
+        this.roomType = roomType;
+        this.roomArea = roomArea;
+        this.acMachine = acMachine;
+    }
+    void display() {
+        System.out.println("Room No :" + roomNo);
+        System.out.println("Room Type :" + roomType);
+        System.out.println("Room Area :" + roomArea);
+        System.out.println("AC Machine :" + acMachine);
+    }
+    public static void main(String[] args) {
+        new Room();
+        Room a1 = new Room(614, "Reading Room", 600.5f, false);
+        a1.display();
+    }
+}
